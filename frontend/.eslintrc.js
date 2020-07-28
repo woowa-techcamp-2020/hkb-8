18578297d224d3a9f2a8c3492866604c0fc5c1a5
blair B <blair441035@gmail.com>
2020-07-28 21:49:44 +0900

    [env][#1]setting ESlint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2020': true,
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 11,
+        'sourceType': 'module',
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4,
+        ],
+        'quotes': [
+            'error',
+            'single',
+        ],
+        'semi': [
+            'error',
+            'always',
+        ],
+        'no-console': 'off',
+        'comma-dangle': [
+            'error',
+            'always',
+        ],
+
+    },
+};
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -10,6 +10,8 @@ import './app.scss';
 // })
 
 
-console.log(math.sum(1,3))
+console.log(math.sum(1,3));
 console.log(sum(6,3))
 console.log(sum(2,1))
+console.log();
+(function() {})()
