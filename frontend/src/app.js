f4c3370c13574a622670d334578f4da47c8c1868
blair B <blair441035@gmail.com>
2020-07-28 17:42:02 +0900

    [env][#1]webpack install and apply loaders: css-loader, style-loader,file-loader, url-loader

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,13 @@
 import * as math from './math.js'
 import { sum } from './math.js'
+import './app.css';
+import bori from './bori.jpg';
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.body.innerHTML = `
+	<img src=${bori} />
+`
+})
 
 console.log(math.sum(1,3))
 console.log(sum(6,3))
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,5 +9,25 @@ module.exports = {
 		path: path.resolve('./dist'), // node의 path모듈을 이용해서 절대경로를 설정해준다.
 		filename: '[name].js' // 번들링 된 파일명입력 []안에는 entry에서 설정한 키값(=여기선 main)이 될 것이다.
 													// entry가 만약 여러개 라면 output을 동적으로 여러개 만들 수 있는 장점이 있다.
+	},
+	module: { // 로더들은 module의 rules에 추가하면 읽힌다.
+		rules: [
+			{
+				test: /\.css$/,  // test : 로더가 처리해야할 파일들의 패턴(정규표현식)
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test:/\.(png|jpg|gif|svg)$/,
+				loader:'url-loader',
+					options: {
+					publicPath:'./dist/', // dist에 떨궈지기
+					name:'[name].[ext]?[hash]', // 결과 파일의 이름
+						limit: 20000, // 용량 2kb미만은 url-loader 그 이상은 file-loader
+					}
+			}
+		]
 	}
 }
