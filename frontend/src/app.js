88dad3e2201e39d2896d0198b33472f75a6bd906
blair B <blair441035@gmail.com>
2020-07-29 21:11:35 +0900

    [feature][#7]arrange files

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,8 +1,14 @@
-import { header } from './component/header/header';
+import { HeaderSection } from './component/header/header';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const targetEl = document.querySelector('#app',);
-    header.createHeader();
-    targetEl.appendChild(header.render());
-});
+const headerSection = new HeaderSection();
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const targetEl = document.querySelector('#app',);
+//     header.createHeader();
+//     targetEl.appendChild(header.render());
+// });
+
+const targetEl = document.querySelector('#app'); // 최상단 노드
+headerSection.createHeader();
+targetEl.appendChild(headerSection.render());
 
