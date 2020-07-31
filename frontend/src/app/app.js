3cbf6418538eecd7e19b0c9dd6a76b97d33e3e72
brightchul <abhbtbb@gmail.com>
2020-07-31 10:34:51 +0900

    [env][#3] 웹팩 설정 변경에 따른 app 디렉토리 분할
    웹팩 설정으로 entry point를 2개 (app, charApp)로 했기 때문에 그중 하나인 app을 따로 디렉토리를 만들었다.

---
+++ frontend/src/app/app.js
@@ -0,0 +1,17 @@
+import { HeaderSection } from '../component/header/header';
+import { ListTap, MonthTap } from '../component/tap/tap';
+
+const headerSection = new HeaderSection();
+const monthTap = new MonthTap();
+const listTap = new ListTap();
+
+const app = document.querySelector('#app'); // 최상단 노드
+headerSection.createHeader();
+app.appendChild(headerSection.render());
+
+monthTap.createMonthTapNodes();
+app.appendChild(monthTap.render());
+
+listTap.createListTap();
+app.appendChild(listTap.render());
+
