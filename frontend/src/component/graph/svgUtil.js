c310f72669a0c02aa24bfc1cc00602ebc82655fd
brightchul <abhbtbb@gmail.com>
2020-07-31 11:15:40 +0900

    [feature][#3] 막대그래프, 파이차트 추가
    막대그래프를 추가했다.
    사이즈 수정을 변수로 빼놓기는 했지만 내부적으로 빼놓은거라 사이즈 수정까지 반영하려면 코드를 약간 수정해야 하는 부분이 남아 있다.
    
    파이차트를 추가했다.
    아직 글자 관련 항목이 추가 구현이 필요하지만
    파이 영역과 파이영역 애니메이션을 같이 추가했다.
    데모 생각해서 부족하지만 추가를 했다.

---
+++ frontend/src/component/graph/svgUtil.js
@@ -0,0 +1,42 @@
+const SVG_URI = "http://www.w3.org/2000/svg";
+
+const createNS = (type, attr, ...children) => {
+    const element = document.createElementNS(SVG_URI, type);
+    for (const name in attr) {
+        if (name === "textContent") {
+            const textNode = document.createTextNode(attr[name]);
+            element.appendChild(textNode);
+        } else {
+            element.setAttributeNS(null, name, attr[name]);
+        }
+    }
+    children.forEach((child) => element.appendChild(child));
+    return element;
+};
+
+export default {
+    createNS,
+    svg(attr, ...children) {
+        const element = createNS("svg", attr, ...children);
+        return element;
+    },
+    g(attr, ...children) {
+        let gElement;
+        return (gElement = createNS("g", attr, ...children));
+    },
+    line(attr) {
+        return createNS("line", attr);
+    },
+    rect(attr, ...children) {
+        return createNS("rect", attr, ...children);
+    },
+    text(attr, textContent) {
+        return createNS("text", Object.assign(attr, { textContent }));
+    },
+    circle(attr, ...children) {
+        return createNS('circle', attr, ...children);
+    },
+    animate(attr) {
+        return createNS("animate", attr);
+    },
+};
