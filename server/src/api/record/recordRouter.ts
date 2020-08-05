a2a12fb62a392085ac067fc9a2913a7cab22c51e
brightchul <abhbtbb@gmail.com>
2020-08-05 17:52:08 +0900

    [feature][#35] record, history api 추가
    POST : /api/record/:email/:month
    해당 월의 거래내역 추가
    
    POST : /api/payment/method/:memberNo/
    사용자의 결제수단 추가
    
    DELETE : /api/payment/method/:memberNo/
    사용자의 결제수단 제거

--- server/src/api/record/recordRouter.ts
+++ server/src/api/record/recordRouter.ts
@@ -4,5 +4,6 @@ import recordController from "./recordController";
 const router = Router();
 
 router.get("/:email/:month", recordController.getMonthRecord);
+router.post("/:email/:month", () => { });  // 개별 내역 추가
 
 export default router;
