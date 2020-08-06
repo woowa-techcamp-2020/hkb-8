a2ee8099542efa361973faeccb9c7eb6e5fbc7b0
brightchul <abhbtbb@gmail.com>
2020-08-06 14:20:24 +0900

    [update][#32] inpuHandler에서 input으로 핸들러 위치 조정
    기존에 inputHanlder.js에 핸들러들이 있었는데 서로 데이터를 받아오고 하기가 힘들었다. 그래서 input 컴포넌트 안에 핸들러를 옮겼다.
    
    옮긴 결과 수입/지출

--- frontend/src/component/body/input/input.js
+++ frontend/src/component/body/input/input.js
@@ -1,15 +1,22 @@
 import './input.scss';
 import { createEl } from '../../../utils/createElement';
 import { appendArray } from '../../../utils/handleElement';
-import { clickDeleteInputInfoBtn, clickIncomeBtn, clickOutcomeBtn, keyupMoneyInput, clickSubmitBtn } from './inputHandler.js';
+import { clickDeleteInputInfoBtn, keyupMoneyInput } from './inputHandler.js';
 import { div, input, select, option } from '../../../utils/element';
 import api from '../../../utils/api';
 
+const INCOME_CLICKED = "수입";
+const OUTCOME_CLICKED = "지출";
 
 export class Input {
     constructor() {
         this.inputSection = createEl('div', 'input-section', '', {});
+        this.inputClassification = INCOME_CLICKED;
+        this.paymentMethods = undefined;
 
+        this.clickIncomeBtn = this.clickIncomeBtn.bind(this);
+        this.clickOutcomeBtn = this.clickOutcomeBtn.bind(this);
+        this.clickSubmitBtn = this.clickSubmitBtn.bind(this);
     }
     setData(data) {
         this.data = data;
@@ -21,7 +28,6 @@ export class Input {
     }
 
     reset() {
-        this.paymentMethods;
         this.inputSection.innerHTML = '';
     }
 
@@ -31,8 +37,8 @@ export class Input {
                 div({ className: 'input-top' },
                     div({ className: 'classification-wrap wrap' },
                         div({ className: 'classification-title title' }, '분류'),
-                        div({ className: 'classification-income classification-btn clicked', onclick: clickIncomeBtn }, '수입'),
-                        div({ className: 'classification-outcome classification-btn', onclick: clickOutcomeBtn }, '지출')),
+                        div({ className: `classification-income classification-btn ${this.getClicked(INCOME_CLICKED)}`, onclick: this.clickIncomeBtn }, '수입'),
+                        div({ className: `classification-outcome classification-btn ${this.getClicked(OUTCOME_CLICKED)}`, onclick: this.clickOutcomeBtn }, '지출')),
                     div({ className: 'delete-input-info-btn', onclick: clickDeleteInputInfoBtn }, '내용 지우기')),
                 div({ className: 'input-middle' },
                     div({ className: 'input-date-wrap wrap' },
@@ -40,12 +46,12 @@ export class Input {
                         input({ className: 'input-date-text text', type: 'date' })),
                     div({ className: 'input-category-wrap wrap' },
                         div({ className: 'input-category-title title' }, '카테고리'),
-                        select({ className: 'input-category-select income-select select' },
+                        select({ className: `input-category-select income-select select ${this.getHidden(INCOME_CLICKED)}` },
                             option({ className: 'option-list', selected: true, disabled: true, hidden: true }, '선택해주세요'),
                             option({ className: 'option-list', value: "1" }, '월급'),
                             option({ className: 'option-list', value: "2" }, '용돈'),
                             option({ className: 'option-list', value: "3" }, '기타수입')),
-                        select({ className: 'input-category-select outcome-select select hidden' },
+                        select({ className: `input-category-select outcome-select select ${this.getHidden(OUTCOME_CLICKED)}` },
                             option({ className: 'option-list', selected: true, disabled: true, hidden: true }, '선택해주세요'),
                             option({ className: 'option-list', value: "4" }, '식비'),
                             option({ className: 'option-list', value: "5" }, '생활'),
@@ -67,12 +73,63 @@ export class Input {
                     div({ className: 'input-content-wrap wrap' },
                         div({ className: 'input-content-title title' }, '내용'),
                         input({ className: 'input-content-text text' })))),
-            div({ className: 'input-submit-btn', onclick: clickSubmitBtn }, '확 인'));
+            div({ className: 'input-submit-btn', onclick: this.clickSubmitBtn }, '확 인'));
 
         return InputBoxWrap;
     }
     submitHandler() {
+    }
+    getHidden(type) {
+        return this.inputClassification === type ? '' : 'hidden';
+    }
+    getClicked(type) {
+        return this.inputClassification === type ? 'clicked' : '';
+    }
+    clickIncomeBtn(e) {
+        this.inputClassification = INCOME_CLICKED;
+        this.toggle();
+    };
 
+    clickOutcomeBtn(e) {
+        this.inputClassification = OUTCOME_CLICKED;
+        this.toggle();
+    };
+    toggle() {
+        const inputSection = this.inputSection;
+        inputSection.querySelector('.classification-income').classList.toggle('clicked');
+        inputSection.querySelector('.classification-outcome').classList.toggle('clicked');
+        inputSection.querySelector('.income-select').classList.toggle('hidden');
+        inputSection.querySelector('.outcome-select').classList.toggle('hidden');
+    }
+
+    clickSubmitBtn(e) {
+        // 결제 날짜
+        const inputSection = this.inputSection;
+        const dateText = inputSection.querySelector('.input-date-text').value;
+        if (!dateText) return alert("날짜가 제대로 입력되지 않았습니다.");
+
+        // 카테고리
+        const category = (this.inputClassification === INCOME_CLICKED)
+            ? inputSection.querySelector('.income-select').value
+            : inputSection.querySelector('.outcome-select').value;
+
+        if (category === "선택해주세요") return alert("카테고리를 선택해 주세요");
+
+        // 결제 수단
+        const paymentMethod = inputSection.querySelector('.payment-method-select').value;
+        if (paymentMethod === "선택해주세요") return alert("결제 수단을 선택해 주세요");
+
+        // money
+        const inputMoney = inputSection.querySelector('.input-money-text').value.match(/\d+/g).join("");
+        const money = parseInt(inputMoney);
+        if (money <= 0 || Number.isNaN(money)) return alert("금액을 입력해 주세요");
+
+        // 내용
+        const content = inputSection.querySelector('.input-content-text').value.trim();
+        if (!content) return alert("내용을 입력해 주세요");
+
+        // api로 보내기
+        window.model.addNewPaymentRecord({ dateText, category, paymentMethod, money, content });
     }
 
     createSelectOptions(data) {
--- frontend/src/component/body/input/inputHandler.js
+++ frontend/src/component/body/input/inputHandler.js
@@ -1,70 +1,9 @@
-let inputClassification = "수입";
-let inputMoney;
-
-// TODO : input에 붙일 것.
-export const clickSubmitBtn = function (e) {
-    // 결제 날짜
-    const dateText = document.querySelector('.input-date-text').value;
-    if (!dateText) return alert("날짜가 제대로 입력되지 않았습니다.");
-
-    // 카테고리
-    const category = (inputClassification === '수입')
-        ? document.querySelector('.income-select').value
-        : document.querySelector('.outcome-select').value;
-
-    if (category === "선택해주세요") return alert("카테고리를 선택해 주세요");
-
-    // 결제 수단
-    const paymentMethod = document.querySelector('.payment-method-select').value;
-    if (paymentMethod === "선택해주세요") return alert("결제 수단을 선택해 주세요");
-
-    // money
-    const money = parseInt(inputMoney);
-    if (money <= 0 || Number.isNaN(money)) return alert("금액을 입력해 주세요");
-
-    // 내용
-    const content = document.querySelector('.input-content-text').value.trim();
-    if (!content) return alert("내용을 입력해 주세요");
-
-    const result = { dateText, category, paymentMethod, money, content };
-    console.log(result);
-
-    // api로 보내기
-    window.model.addNewPaymentRecord(result);
-    // 그 이후 결과값 받기
-
-    // 모델에 보내야 함. 
-}
+import { numberFormat } from '../../../utils/etc';
 
 export const clickDeleteInputInfoBtn = function (e) {
     // console.log(e);
 };
 
-export const clickIncomeBtn = function (e) {
-    inputClassification = "수입";
-    console.log("in " + inputClassification);
-
-    e.target.classList.add('clicked');
-    document.querySelector('.classification-outcome').classList.remove('clicked');
-
-    const incomeSelect = document.querySelector('.income-select');
-    incomeSelect.classList.remove('hidden');
-    const outcomeSelect = document.querySelector('.outcome-select');
-    outcomeSelect.classList.add('hidden');
-};
-
-export const clickOutcomeBtn = function (e) {
-    inputClassification = "지출";
-    console.log("out " + inputClassification);
-
-    e.target.classList.add('clicked');
-    document.querySelector('.classification-income').classList.remove('clicked');
-
-    const incomeSelect = document.querySelector('.income-select');
-    incomeSelect.classList.add('hidden');
-    const outcomeSelect = document.querySelector('.outcome-select');
-    outcomeSelect.classList.remove('hidden');
-};
 
 const TEXT_LENGTH = 15;
 export const keyupMoneyInput = function (e) {
@@ -76,10 +15,3 @@ export const keyupMoneyInput = function (e) {
     inputMoney = numText;
     e.target.value = numText ? numberFormat(numText) : "";
 }
-
-const numberFormatkoKr = new Intl.NumberFormat('ko-KR');
-
-function numberFormat(num) {
-    return numberFormatkoKr.format(num);
-}
-
